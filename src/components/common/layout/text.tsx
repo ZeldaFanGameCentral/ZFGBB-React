09a83965c564de1fa3d1fc2e03d5fe2d50c0b43b
Jonathan Basniak <740416+gm112@users.noreply.github.com>
2025-10-17 02:36:25 -0400

    chore: move text component to different folder
    Signed-off-by: Jonathan Basniak <740416+gm112@users.noreply.github.com>

--- src/components/common/layout/text.tsx
+++
@@ -1,4 +0,0 @@
-import type React from "react";
-export const MutedText = ({ children }: { children: React.ReactNode }) => (
-  <span className="text-muted">{children}</span>
-);
---
+++ src/components/text/muted.component.tsx
@@ -0,0 +1,8 @@
+import type React from "react";
+export const MutedText = ({
+  children,
+  Ass = "span",
+}: {
+  children: React.ReactNode;
+  Ass?: React.ElementType;
+}) => <Ass className="text-muted">{children}</Ass>;
