f532a86092c2f72f7955367e4709ec507e7b2e0f
Jonathan Basniak <740416+gm112@users.noreply.github.com>
2025-10-19 23:31:51 -0400

    feat(core): preprocess classNames at build time
    Signed-off-by: Jonathan Basniak <740416+gm112@users.noreply.github.com>

--- core/vite/plugins/preprocess-twmerge/src/plugin.ts
+++ core/vite/plugins/preprocess-twmerge/src/plugin.ts
@@ -1,16 +1,107 @@
 import type { Plugin } from "vite";
 
+import { twMerge } from "tailwind-merge";
+import { parse } from "@babel/parser";
+import traverse from "@babel/traverse";
+import generate from "@babel/generator";
+import {
+  isArrayExpression,
+  isCallExpression,
+  isConditionalExpression,
+  isIdentifier,
+  isJSXExpressionContainer,
+  isLogicalExpression,
+  isStringLiteral,
+  isTemplateLiteral,
+  stringLiteral,
+  type Node,
+} from "@babel/types";
+
+function mergeClassesFromExpression(node: Node): string {
+  if (isStringLiteral(node)) return node.value;
+
+  if (isTemplateLiteral(node))
+    return node.quasis.map((q) => q.value.raw).join(" ");
+
+  if (isConditionalExpression(node))
+    return [
+      mergeClassesFromExpression(node.consequent),
+      mergeClassesFromExpression(node.alternate),
+    ].join(" ");
+
+  if (isLogicalExpression(node))
+    return [mergeClassesFromExpression(node.right)].join(" ");
+
+  if (isArrayExpression(node))
+    return node.elements
+      .map((element) =>
+        element ? mergeClassesFromExpression(element as Node) : "",
+      )
+      .join(" ");
+
+  const node_is_clsx_or_classnames =
+    isCallExpression(node) &&
+    isIdentifier(node.callee) &&
+    ["clsx", "classnames"].includes(node.callee.name);
+
+  if (!node_is_clsx_or_classnames) return "";
+
+  return node.arguments
+    .map((arg) => mergeClassesFromExpression(arg as Node))
+    .join(" ");
+}
+
+const preprocessTwMergeOptions = {
+  include: /\.(jsx|tsx)$/,
+  exclude: /node_modules/,
+};
+
+export type PreprocessTwMergeOptions = Partial<typeof preprocessTwMergeOptions>;
+
 /**
  * This plugin will preprocess tailwind merge on React components, so that it
  * does not have to be done at runtime.
  * @returns
  */
-export function preprocessTwMerge(): Plugin[] {
+export function preprocessTwMerge({
+  include = preprocessTwMergeOptions.include,
+  exclude = preprocessTwMergeOptions.exclude,
+}: PreprocessTwMergeOptions): Plugin[] {
   return [
     {
       name: "vite-plugin-preprocess-twmerge",
       enforce: "pre",
-      transform(code, id) {},
+      transform(code, id) {
+        if (!include.test(id) || exclude.test(id)) return;
+
+        const ZeldaAncientStoneTablets = parse(code, {
+          sourceType: "module",
+          plugins: ["jsx", "typescript"],
+        }); //babel AST
+
+        traverse(ZeldaAncientStoneTablets, {
+          JSXAttribute(path) {
+            if (path.node.name.name !== "className") return;
+
+            let expr = path.node.value;
+            if (!expr) return;
+
+            // className="..."
+            if (isStringLiteral(expr)) {
+              path.node.value = stringLiteral(twMerge(expr.value));
+            }
+
+            // className={...}
+            if (isJSXExpressionContainer(expr)) {
+              const rawClasses = mergeClassesFromExpression(expr.expression);
+              path.node.value = stringLiteral(twMerge(rawClasses));
+            }
+          },
+        });
+
+        const result = generate(ZeldaAncientStoneTablets, {}, code);
+        return { code: result.code, map: null };
+      },
     },
   ];
 }
