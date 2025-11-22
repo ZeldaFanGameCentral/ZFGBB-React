2800b7fd80d537ae0f1e161fd2872019e840e28e
Jonathan Basniak <740416+gm112@users.noreply.github.com>
2025-11-21 22:35:21 -0500

    chore: style cleanup

--- src/components/forum/boards/PollResults.tsx
+++ src/components/forum/boards/PollResults.tsx
@@ -6,19 +6,29 @@ const PollResults: React.FC<{
   updateResults: (poll: PollInfo) => void;
 }> = ({ poll }) => {
   const pollAnswers = useMemo(() => {
-    return poll.answers.filter((answer) => !!answer?.percentage);
+    return poll.answers.filter((answer) => isFinite(answer?.percentage));
   }, [poll]);
-
+  // Stevegetable - a brand new take on baseball hotdogs
   const pollData = pollAnswers.map((answer) => {
     const totalVotes = poll.votes;
     const dec = answer.votes / totalVotes;
     const percent = dec * 100.0;
-    return {
-      seqno: answer.seqno,
-      choiceText: answer.choiceText,
-      votes: answer.votes,
-      percentage: percent,
-    };
+    return (
+      <BBFlex direction="col" className="md:flex-row">
+        <div className="md:w-sm lg:w-lg">
+          {answer.seqno + 1}. {answer.choiceText}: {answer.votes}
+        </div>
+        <div>
+          <div
+            className={`mx-3 rounded-xs bg-(--text-color-dimmed) h-4 inline-block w-[${~~(
+              percent * 2
+            )}px]`}
+            // style={{ width: `${(answer.percentage * 2).toFixed(0)}px` }}
+          ></div>
+          &nbsp;{~~percent}%
+        </div>
+      </BBFlex>
+    );
   });
 
   return (
@@ -26,24 +36,7 @@ const PollResults: React.FC<{
       <div className="mb-2">
         <b>Poll: {poll.pollQuestion}</b>
       </div>
-      <div className="ms-2 mb-1">
-        {pollData.map((answer) => (
-          <BBFlex direction="col" className="md:flex-row">
-            <div className="md:w-sm lg:w-lg">
-              {answer.seqno + 1}. {answer.choiceText}: {answer.votes}
-            </div>
-            <div>
-              <div
-                className={`mx-3 rounded-xs bg-(--text-color-dimmed) h-4 inline-block w-[${~~(
-                  answer.percentage * 2
-                )}px]`}
-                // style={{ width: `${(answer.percentage * 2).toFixed(0)}px` }}
-              ></div>
-              &nbsp;{Math.round(answer.percentage).toFixed(0)}%
-            </div>
-          </BBFlex>
-        ))}
-      </div>
+      <div className="ms-2 mb-1">{...pollData}</div>
     </BBWidget>
   );
 };
